28d1cb6485597da227302b6702e975856e0838d4
Stefan Dzisiewski-Smith <stefan@bareconductive.com>
2016-10-04 16:13:06 +0100

    Initial public commit

---
+++ simple-mqtt.py
@@ -0,0 +1,88 @@
+################################################################################
+#
+# Bare Conductive Pi Cap
+# ----------------------
+#
+# simple-mqtt.py - sends capacitive touch / release data from MPR121 to a
+# specified MQTT broker.
+#
+# Written for Raspberry Pi.
+#
+# Original example by Sven Haiges.
+#
+# Bare Conductive code written by Szymon Kaliski.
+#
+# This work is licensed under a Creative Commons Attribution-ShareAlike 3.0
+# Unported License (CC BY-SA 3.0) http://creativecommons.org/licenses/by-sa/3.0/
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+#
+#################################################################################
+
+from time import sleep
+from mosquitto import Mosquitto
+import signal, sys, getopt, MPR121
+
+try:
+  sensor = MPR121.begin()
+except Exception as e:
+  print e
+  sys.exit(1)
+
+# handle ctrl+c gracefully
+def signal_handler(signal, frame):
+  sys.exit(0)
+
+signal.signal(signal.SIGINT, signal_handler)
+
+# print help
+def print_help():
+  print "Sends Pi Cap touch readings through MQTT - MUST be run as root.\n"
+  print "Usage: python simple-mqtt.py [OPTIONS]\n"
+  print "Options:"
+  print "  -b, --broker  MQTT broker [REQUIRED]"
+  print "      --help    displays this message"
+  sys.exit(0)
+
+broker = ""
+
+# arguments parsing
+def parse_args(argv):
+  # we need to tell python that those variables are global
+  # we don't want to create new local copies, but change global state
+  global broker
+
+  try:
+    opts, args = getopt.getopt(argv, "b:", [ "broker=", "help" ])
+  except getopt.GetoptError:
+    print_help()
+
+  for opt, arg in opts:
+    if opt in ("-b", "--broker"):
+      broker = arg
+    elif opt in ("--help"):
+      print_help()
+
+# parse arguments on start
+parse_args(sys.argv[1:])
+
+# setup MQTT
+client = Mosquitto()
+client.connect("mqtt://" + broker)
+
+while True:
+  if sensor.touch_status_changed():
+    sensor.update_touch_data()
+    for i in range(12):
+      if sensor.is_new_touch(i):
+        client.publish("picap/touched", i)
+      elif sensor.is_new_release(i):
+        client.publish("picap/released", i)
+
+  sleep(0.01)
